44810b045e34a6f463e992e9ed8ce3dd27a1e22b
linuxdaemon <linuxdaemon.irc@gmail.com>
2020-05-24 23:09:47 -0400

    Fix bot client tests

--- tests/core_tests/irc_client_test.py
+++ tests/core_tests/irc_client_test.py
@@ -1,8 +1,8 @@
 import asyncio
 import logging
 import socket
-from asyncio import Task
-from unittest.mock import MagicMock, call, patch
+from unittest.mock import call, patch
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -17,12 +17,10 @@ class TestLineParsing:
         return {k: v for k, v in dict(event).items() if not callable(v)}
 
     def test_data_received(self):
-        conn, out, proto = self.make_proto()
+        _, out, proto = self.make_proto()
         proto.data_received(
             b":server.host COMMAND this is :a command\r\n:server.host PRIVMSG me :hi\r\n"
         )
-
-        conn.loop.run_until_complete(asyncio.gather(*Task.all_tasks(conn.loop)))
 
         assert out == [
             {
@@ -70,10 +68,10 @@ class TestLineParsing:
     def make_proto(self):
         conn = MagicMock()
         conn.nick = "me"
-        conn.loop = asyncio.get_event_loop_policy().new_event_loop()
+        conn.loop = conn.bot.loop = asyncio.get_event_loop_policy().new_event_loop()
         out = []
 
-        async def func(e):
+        def func(e):
             out.append(self._filter_event(e))
 
         conn.bot.process = func
@@ -81,12 +79,10 @@ class TestLineParsing:
         return conn, out, proto
 
     def test_broken_line_doesnt_interrupt(self):
-        conn, out, proto = self.make_proto()
+        _, out, proto = self.make_proto()
         proto.data_received(
             b":server.host COMMAND this is :a command\r\nPRIVMSG\r\n:server.host PRIVMSG me :hi\r\n"
         )
-
-        conn.loop.run_until_complete(asyncio.gather(*Task.all_tasks(conn.loop)))
 
         assert out == [
             {
